d561d318aa59009399c165f4bb5e04d34bafc730
Joakim Sjöström <8036138+J0kk3@users.noreply.github.com>
2024-03-25 13:05:10 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+import "./App.css";
+
+function App ()
+{
+
+  return (
+    <div class="background">
+      <div class="top-banner">
+        This goes behind the other divs
+      </div>
+      <div class="foreground">
+        <div><h1>1</h1></div>
+        <div><h1>2</h1></div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
